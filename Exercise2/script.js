a00ac111ded07b133b1ef523df0af34de765571e
Kevin Sartorelli <kevin.sartorelli@gmail.com>
2023-03-25 20:48:40 +1300

    Start of Exercise 2

--- Exercise2/script.js
+++ Exercise2/script.js
@@ -3,9 +3,10 @@ const form = document.querySelector("form");
 
 form.addEventListener("submit", (e) => {
 	e.preventDefault();
-	localStorage.setItem("name", e.target[0].value);
+	localStorage.setItem("item", e.target[0].value);
 });
 
+/*
 const getData = async () => {
 	const res = await axios.get("https://jsonplaceholder.typicode.com/todos/1");
 
@@ -18,3 +19,4 @@ window.addEventListener("load", (event) => {
 	const name = localStorage.getItem("name");
 	h1.innerText = name;
 });
+*/
