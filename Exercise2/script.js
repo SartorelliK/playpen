2c75ad22e4b307d19ef55bfb024c3af9d2270cca
Kevin Sartorelli <kevin.sartorelli@gmail.com>
2023-03-28 20:34:38 +1300

    Just need to get delete working now

--- Exercise2/script.js
+++ Exercise2/script.js
@@ -1,12 +1,10 @@
-const h1 = document.querySelector("h1");
-const h3 = document.querySelector("h3");
 const form = document.querySelector("form");	// CAN WE DO THIS WITH AN ID?
 const div = document.querySelector("#listed");	// div that holds the list of todo items
 
 // From https://github.com/ai/nanoid  - Gives a rndom UUID of 21 characters
 let nanoid=(t=21)=>crypto.getRandomValues(new Uint8Array(t)).reduce(((t,e)=>t+=(e&=63)<36?e.toString(36):e<62?(e-26).toString(36).toUpperCase():e>62?"-":"_"),"");
 
-let todos = []
+let todos = []									// Define our array
 
 // Output the todo items
 function listtodos(s) {
@@ -17,19 +15,17 @@ function listtodos(s) {
 	var i = 0;
 	while(i < sarray.length) {					// Go through the entire array
 		formString = '<input type="radio" id="' + sarray[i].id + '" value="' + sarray[i].text + '">'
-		formString += '<label for="' + sarray[i].id + '">' + sarray[i].text + '</label><br />'
+		formString += ' <label for="' + sarray[i].id + '">' + sarray[i].text + '</label><br />'
 //		console.log(formString)					// Display on console for debugging
 		var p = document.createElement("p"); 	// Create a <p> element
-		var t = document.createTextNode(formString); // Create a text node
-		p.appendChild(t);
+        p.innerHTML = formString;				// Put the HTML into it
 		document.getElementById("listed").appendChild(p); // Output to element called 'listed'
-		i++
+		i++										// To next item in array
 	};
-// REFRESH FORM NEEDS TO GO HERE
 }
 
-// Get todo just enterd when user hits submit and add to array of others.
-// Put in local storage to isstays between reloads
+// Get todo just entered when user hits submit and add to array of others.
+// Put in local storage so it stays between reloads
 form.addEventListener("submit", (e) => {
 	e.preventDefault();
 	todo = {									// Set up this item
@@ -42,18 +38,15 @@ form.addEventListener("submit", (e) => {
 
 //    console.log(stringed)						// Display on console for debugging
 
-//	const stringed1 = localStorage.getItem("todos"); // To get items from local storage
-//	todos = JSON.parse(stringed1)
-
     listtodos(stringed)							// Output the array as entries in a radio button form
 });
 
-/*
-
-getData();
-
-window.addEventListener("load", (event) => {
-	const name = localStorage.getItem("todos");
-	h1.innerText = name;
-});
-*/
+// Load our array from localstorage when we load the page
+window.onload = function() {
+	const stringed = localStorage.getItem("todos"); // To get items from local storage
+//    console.log(stringed);						// Display on console for debugging
+    if (stringed !== null) {						// Check we have something
+        listtodos(stringed)							// Output the array as entries in a radio button form
+        todos = JSON.parse(stringed);				// Turn into an array
+	}
+};
